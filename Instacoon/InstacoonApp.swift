09b88942af4a886cf85e6d83567a028e724e24f6
Anthony Jean <anthony.jean@netc.eu>
2022-12-23 17:26:39 +0100

    Initial Commit

---
+++ Instacoon/InstacoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstacoonApp.swift
+//  Instacoon
+//
+//  Created by Anthony Jean on 23/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct InstacoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
